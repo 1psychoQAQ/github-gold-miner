062eedb1f553f53d29cbd4f2f66c29f0dd9e2fc9
1pschoQAQ <liujh1265684394@gmail.com>
2025-12-17 18:29:37 +0800

    修改运行脚本

--- scripts/run_mining.sh
+++ scripts/run_mining.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897
+# 设置代理
+export https_proxy=http://127.0.0.1:7897
+export http_proxy=http://127.0.0.1:7897
+
+
 # 进入项目目录
 cd /Users/liujiahao/GolandProjects/github-gold-miner
 
-# 执行挖矿任务
-./bin/github-gold-miner -mode=mine
+# 执行挖矿任务，每30分钟一次，5个并发
+./bin/github-gold-miner -mode=mine -interval=30 -concurrency=5
 
 echo "$(date): 挖矿任务执行完成" >> /var/log/github-gold-miner.log
